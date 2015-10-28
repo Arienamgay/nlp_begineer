0a4f981ce7e64f4963271c920cd33fb962f3ca59
arienamgay <phub_namgay2001@yahoo.com>
2015-10-28 22:09:34 +0600

    Added text to Readme file

--- dzongkha.py
+++ dzongkha.py
@@ -1,9 +1,15 @@
-import nltk
-import codecs
+#python code to read Dzongkha text file
 from nltk.corpus import PlaintextCorpusReader
-corpus_root='C:/Users/Arie Namgay/Desktop/nlp/Dzongkha'
-wordlists = PlaintextCorpusReader(corpus_root, 'story-Eng.txt')
+corpus_root='C:/Users/Arie Namgay/Documents/GitHub/Dzongkha'
+wordlists = PlaintextCorpusReader(corpus_root, 'story.txt')
 print(wordlists)
 dzongkha=wordlists.words()
-for word in dzongkha:
-    print(word),
+length=len(dzongkha)
+print("The total no of words in the text is: %d" % length)
+print("\n")
+for index, item in enumerate(dzongkha):
+	print(item, end=" "),
+	if(dzongkha[index]=="}*"):
+		print('\n')
+print("\n")
+
